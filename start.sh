5775d41dd0792f315010827f770518b0cc909b13
Viktor Pokhilko <viktorpokhilko@sataninja.local>
2019-06-24 12:11:22 +0300

    v 1.0

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# get jar/config folder location
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+#set default folder to provide the base path for logging.
+CD "$DIR"
